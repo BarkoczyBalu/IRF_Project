e14a292d439e64c59300ea4488698eeee72d1dda
Balázs Barkóczy <balazs.barkoczy@stud.uni-corvinus.hu>
2020-12-03 22:55:57 +0100

    XMLRead

---
+++ CovidCounter/Entities/Record.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovidCounter.Entities
+{
+    class Record
+    {
+        public string Date { get; set; }
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+        public int Cases { get; set; }
+        public int Deaths { get; set; }
+        public string Countries { get; set; }
+        public string GeoID { get; set; }
+        public string CountryCode { get; set; }
+        public string Continent { get; set; }
+        public double Cumulative_number { get; set; }
+        //Kummulált érték 2 heti esetszám alpján 100 000 főre nézve 
+    }
+}
--- CovidCounter/Form1.cs
+++ CovidCounter/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CovidCounter.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace CovidCounter
 {
     public partial class Form1 : Form
     {
+        BindingList<Record> Records = new BindingList<Record>();
         public Form1()
         {
             InitializeComponent();
+            CreateXML();
+            dataGridView1.DataSource = Records;
+        }
+
+        void CreateXML()
+        {
+            var xml = new XmlDocument();
+            xml.Load("Covid Cases.xml");
+
+            foreach (XmlElement element in xml.DocumentElement)
+            {
+                var rec = new Record();
+                Records.Add(rec);
+
+                var date = (XmlElement)element.ChildNodes[0];
+                if (date.Name == "dateRep") { rec.Date = date.InnerText; }
+
+                var day = (XmlElement)element.ChildNodes[1];
+                if (day.Name == "day") { rec.Day = int.Parse(day.InnerText); }
+
+                var month = (XmlElement)element.ChildNodes[2];
+                if (month.Name == "month") { rec.Month = int.Parse(month.InnerText); }
+
+                var year = (XmlElement)element.ChildNodes[3];
+                if (year.Name == "year") { rec.Year = int.Parse(year.InnerText); }
+
+                var cases = (XmlElement)element.ChildNodes[4];
+                if (cases.Name == "cases") { rec.Cases = int.Parse(cases.InnerText); }
+
+                var deaths = (XmlElement)element.ChildNodes[5];
+                if (deaths.Name == "deaths") { rec.Deaths = int.Parse(deaths.InnerText); }
+
+                var countriesAndTerritories = (XmlElement)element.ChildNodes[6];
+                if (countriesAndTerritories.Name == "countriesAndTerritories") { rec.Countries = countriesAndTerritories.InnerText; }
+
+                var geoId = (XmlElement)element.ChildNodes[7];
+                if (geoId.Name == "geoId") { rec.GeoID = geoId.InnerText; }
+
+                var countryterritoryCode = (XmlElement)element.ChildNodes[8];
+                if (countryterritoryCode.Name == "countryterritoryCode") { rec.CountryCode = countryterritoryCode.InnerText; }
+
+                var continentExp = (XmlElement)element.ChildNodes[9];
+                if (continentExp.Name == "continentExp") { rec.Continent = continentExp.InnerText; }
+
+                var Cumulative = (XmlElement)element.ChildNodes[10];
+                if (Cumulative.Name == "Cumulative_number_for_14_days_of_COVID-19_cases_per_100000") { rec.Cumulative_number = double.Parse(Cumulative.InnerText); }
+
+                //if (element == null) { continue; }
+            }
         }
     }
 }
